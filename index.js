14b38caa0ea5dc6c31330ceb8fa64c15141b2dad
Priyanka_1081 <70508020+priyanka1081@users.noreply.github.com>
2021-01-12 20:08:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+document.querySelectorAll(".drum");
+for(var i=0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var key=this.innerHTML;
+        handleClick(key);
+        animate(key);
+    });
+    
+}
+document.addEventListener("keypress",function(event){
+    handleClick(event.key);
+    animate(event.key);
+});
+
+function animate(key){
+    var buttonPress=document.querySelector("."+key);
+    buttonPress.classList.add("pressed");
+    setTimeout(function(){
+        buttonPress.classList.remove("pressed");
+    }, 100);
+}
+
+function handleClick(key){
+    switch (key) {
+        case "w":new Audio("sounds/tom-1.mp3").play(); 
+            break;
+        case "a":new Audio("sounds/tom-2.mp3").play();            
+            break;
+        case "s":new Audio("sounds/tom-3.mp3").play();            
+            break;
+        case "d":new Audio("sounds/tom-4.mp3").play();            
+            break;
+        case "j":new Audio("sounds/snare.mp3").play();            
+            break;
+        case "k":new Audio("sounds/crash.mp3").play();            
+            break;
+        case "l":new Audio("sounds/kick-bass.mp3").play();            
+            break;
+        default: console.log(this.innerHTML + " not in the keys");
+    }
+}
+
+
